2c9bdd2c7bba7678dc45786ac5c2e6261140d0b2
Uma Zalakain <uma@gisa-elkartea.org>
2019-02-01 20:30:32 +0000

    It's a ring!

--- src/Language/ADTrees.hs
+++ src/Language/ADTrees.hs
@@ -13,12 +13,12 @@ invert Opponent = Proponent
 
 -- Domain for a given faction
 data Domain a b = MkDomain
-    { get        :: a -> b
-    , or         :: b -> b -> b
-    , orNeutral  :: b
-    , and        :: b -> b -> b
-    , andNeutral :: b
-    , counter    :: b -> b
+    { get   :: a -> b
+    , plus  :: b -> b -> b
+    , zero  :: b
+    , times :: b -> b -> b
+    , one   :: b
+    , minus :: b -> b
     }
 
 -- Attack-defense domains
@@ -35,9 +35,9 @@ data Event a
 aggregate :: ADDomain a b -> Faction -> Event a -> b
 aggregate d p (Basic _ a) = get (d p) a
 aggregate d p (Comment _ e) = aggregate d p e
-aggregate d p (Counter e) = counter (d p) (aggregate d (invert p) e)
-aggregate d p (And es) = foldr (and (d p) . aggregate d p) (andNeutral $ d p) es
-aggregate d p (Or es) = foldr (or (d p) . aggregate d p) (orNeutral $ d p) es
+aggregate d p (Counter e) = minus (d p) (aggregate d (invert p) e)
+aggregate d p (And es) = foldr (times (d p) . aggregate d p) (one $ d p) es
+aggregate d p (Or es) = foldr (plus (d p) . aggregate d p) (zero $ d p) es
 
 cutsets :: Event a -> [Event a]
 cutsets (Basic n a) = [Basic n a]
@@ -50,15 +50,15 @@ cutsets (Or es) = concatMap cutsets es
 
 probability :: (a -> Rational) -> ADDomain a Rational
 probability f _ = MkDomain
-    { get        = f
-    , or         = (+)
-    , orNeutral  = 0
-    , and        = (*)
-    , andNeutral = 1
-    , counter    = (1 -)
+    { get   = f
+    , plus  = (+)
+    , zero  = 0
+    , times = (*)
+    , one   = 1
+    , minus = (1 -)
     }
 
-data Difficulty = L | M | H
+data Difficulty = L | M | H deriving (Show, Eq)
 
 min :: Difficulty -> Difficulty -> Difficulty
 min L _ = L
@@ -77,58 +77,48 @@ max a b = min (invertDifficulty a) (invertDifficulty b)
 
 difficulty :: (a -> Difficulty) -> ADDomain a Difficulty
 difficulty f Proponent = MkDomain
-    { get        = f
-    , or         = min
-    , orNeutral  = H
-    , and        = max
-    , andNeutral = L
-    , counter    = invertDifficulty
+    { get   = f
+    , plus  = min
+    , zero  = H
+    , times = max
+    , one   = L
+    , minus = invertDifficulty
     }
 difficulty f Opponent = MkDomain
-    { get        = f
-    , or         = max
-    , orNeutral  = L
-    , and        = min
-    , andNeutral = H
-    , counter    = invertDifficulty
+    { get   = f
+    , plus  = max
+    , zero  = L
+    , times = min
+    , one   = H
+    , minus = invertDifficulty
     }
 
 -- TODO: Cost
 
-example :: Event Rational
-example = Comment "Enemy forges package" (Or [ 
-    Comment "Enemy signs forged package" (
-        Comment "Enemy has secret key" (
-            Or [
-                Basic "Enemy has stolen secret key" 0.2,
-                Comment "Enemy has calculated secret key" (
-                    Or [
-                        Basic "Poor key generation allows secret key to be easily derived" 0.1,
-                        Basic "Unknown vulnerability in hash algorithm is exploited" 0.1,
-                        Comment "Enemy uses known attack on weak hash algorithm" (
-                            And [
-                                Basic "Enemy applies known attack to hash algorithm" 0.5,
-                                Basic "Poor choice of hash algorithm" 0.1
-                            ]
-                        )
-                    ]
-                )
-            ]
-        )
-    ),
-    Comment "Enemy replaces genuine archive with malicious archive" (
-        Comment "Enemy has constructed malicious archive with same hash as genuine archive" (
-            Comment "Enemy has effectively broken hash algorithm" (
-                Or [
-                    Basic "Unknown vulnerability in hash algorithm is exploited" 0.1,
-                    Comment "Enemy uses known attack on weak hash algorithm" (
-                        And [
-                            Basic "Enemy applies known attack to hash algorithm" 1.0,
-                            Basic "Poor choice of hash algorithm" 0.1
-                        ]
-                    )
-                ]
-            )
-        )
-    )
-    ])
+data ExampleAttribute = MEA
+    { getProbability :: Rational
+    , getDifficulty  :: Difficulty
+    }
+
+example :: Event ExampleAttribute
+example = Comment "Bank Account" (Or [
+    Comment "ATM" (And [
+        Comment "PIN" (Or [
+            Basic "Eavesdrop" $ MEA 0.01 L,
+            Comment "Find Note" (Counter (
+                Basic "Memorize" $ MEA 0.80 L)),
+            Basic "Force" $ MEA 0.01 H]),
+        Basic "Card" $ MEA 0.1 H]),
+    Comment "Online" (And [
+        Comment "Password" (Or [
+            Basic "Phishing" $ MEA 0.90 L,
+            Basic "Key logger" $ MEA 0.20 M]),
+        Basic "User name" $ MEA 0.90 L,
+        Counter (
+            Comment "2nd Auth Factor" (Or [
+                Basic "Key Fobs" $ MEA 0.01 L,
+                Basic "PIN Pad" $ MEA 0.01 L,
+                Counter (
+                    Comment "Malware" (Or [
+                        Basic "Browser" $ MEA 0.20 M,
+                        Basic "OS" $ MEA 0.20 M]))]))])])
