7bce2ca32ee7c91adb3bd97ed2168470781c81ed
Uma Zalakain <uma@gisa-elkartea.org>
2019-02-13 20:49:24 +0000

    Slightly better example

--- report/examples/BankAccount.hs
+++ report/examples/BankAccount.hs
@@ -4,34 +4,32 @@ import System.Process (runCommand, waitForProcess)
 
 import Language.ADTrees
 
-data ExampleAttribute = MEA
-    { getProbability :: Rational
-    , getDifficulty  :: Difficulty
-    } deriving (Eq)
+type ExampleAttribute = (Rational, Difficulty)
 
 fsExampleAttribute :: ExampleAttribute -> String
-fsExampleAttribute a = printf "probability: %s <br/> difficulty: %s" (show $ getProbability a) (show $ getDifficulty a)
+fsExampleAttribute (p, d) = printf "probability: %s <br/> difficulty: %s" (show p) (show d)
 
 example :: Event ExampleAttribute
-example = Or A "Bank Account" [
-    And A "ATM" [
-        Or A "PIN" [
-            Basic A "Eavesdrop" $ MEA 0.01 L,
-            And A "Find Note" [
-                Basic D "Memorize" $ MEA 0.80 L],
-            Basic A "Force" $ MEA 0.01 H],
-        Basic A "Card" $ MEA 0.1 H],
-    And A "Online" [
-        Or A "Password" [
-            Basic A "Phishing" $ MEA 0.90 L,
-            Basic A "Key logger" $ MEA 0.20 M],
-        Basic A "User name" $ MEA 0.90 L,
-        Or D "2nd Auth Factor" [
-            Basic D "Key Fobs" $ MEA 0.01 L,
-            Basic D "PIN Pad" $ MEA 0.01 L,
-            Or A "Malware" [
-                Basic A "Browser" $ MEA 0.20 M,
-                Basic A "OS" $ MEA 0.20 M]]]]
+example = 
+    Or A "Bank Account" [
+        And A "ATM" [
+            Or A "PIN" [
+                Basic A "Eavesdrop" (0.01, L),
+                And A "Find Note" [
+                    Basic D "Memorize" (0.80, L)],
+                Basic A "Force" (0.01, H)],
+            Basic A "Card" (0.1, H)],
+        And A "Online" [
+            Or A "Password" [
+                Basic A "Phishing" (0.90, L),
+                Basic A "Key logger" (0.20, M)],
+            Basic A "User name" (0.90, L),
+            Or D "2nd Auth Factor" [
+                Basic D "Key Fobs" (0.01, L),
+                Basic D "PIN Pad" (0.01, L),
+                Or A "Malware" [
+                    Basic A "Browser" (0.20, M),
+                    Basic A "OS" (0.20, M)]]]]
 
 main :: IO ()
 main = do args <- getArgs
