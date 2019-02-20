0dd5a0614f2f5dde5cf5ffd1ceb6f0ceeee2382a
Uma Zalakain <uma@gisa-elkartea.org>
2019-02-20 15:40:57 +0000

    Start with the evaluation

--- report/examples/DNSTransactions.hs
+++ report/examples/DNSTransactions.hs
@@ -7,37 +7,40 @@ import Language.ADTrees
 
 example :: ADTree ()
 example =
-    Or "DNS Transactions" [
-        Or "Query/Response Threats" [
-            Or "Forged response" [
-                Or "Compromised server" [
-                    Basic "OS compromised" (),
-                    Basic "Communication stack compromised" ()],
-                Or "Caching server compromised" [
-                    Basic "Packet interception" (),
-                    Basic "Query prediction" ()]],
-            Or "Removal of RRs" [],
-            Or "Incorrect expansion" []
-        ],
-        Or "Zone Transfer Threats" [
-            Or "Denial of Service" [],
-            Or "Tampered transfer message" []
-        ],
-        Or "Dynamic Updates Threats" [
-            Or "Unauthorized updates" [],
-            Or "Tampered update request" [],
-            Or "Replay attacks" []
-        ],
-        Or "DNS NOTIFY Threats" [
-            Or "Spurious NOTIFY" []
-        ]
-    ]
-
-        
+    Or "Attack DNS hosting" [
+        Or "Host platform threads" [
+            Counter "OS"
+                (Or "Attack OS" [
+                    Basic "Vulnerate OS" (),
+                    Basic "Corrupt system config" ()])
+                (And "Protect OS" [
+                    Basic "Patch OS" (),
+                    Basic "Follow NIST's recomm" ()]),
+            Counter "DNS server"
+                (Or "Attack DNS server" [
+                    Basic "Vulnerate DNS server" (),
+                    Basic "Corrupt DNS config" ()])
+                (And "Protect DNS server" [
+                    Basic "Run latest" (),
+                    Basic "Keep config updated" (),
+                    Basic "Review vulnerabilities" (),
+                    Basic "Turn off Version Query" (),
+                    Basic "Run with basic privileges" (),
+                    Basic "Run isolated" (),
+                    Basic "Do not recurse" ()]),
+            Basic "DoS" (),
+            Basic "ARP spoofing" (),
+            Basic "Attack system config" (),
+            Basic "Attack DNS config" (),
+            Basic "MiTM on LAN" ()],
+        Or "DNS data threats" [
+            Basic "Lame delegation" (),
+            Basic "Zone drift and zone thrash" (),
+            Basic "Leak information" ()]]
 
 main :: IO ()
 main = do args <- getArgs
           writeFile (args !! 0) (dot (const " ") A example)
-          pid <- runCommand $ printf "dot -Gsize=\"16.52,11.68\" -Gratio=\"fill\"  -Glandscape=false -Gsplines=ortho -Tpng -v \"%s\" -o\"%s\"" (args !! 0) (args !! 1)
+          pid <- runCommand $ printf "dot -Gratio=\"fill\" -Gsize=\"11.7,8.3!\" -Glandscape=true -Tpng -v \"%s\" -o\"%s\"" (args !! 0) (args !! 1)
           waitForProcess pid
           return ()
